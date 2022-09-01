cd66a94c8d28f5a2283ed0336e5b036715df9484
Dhanwant <dhanwant.nitj@gmail.com>
2022-09-02 00:17:46 +0530

    Player Features

--- Assets/Scripts/FireBullet.cs
+++ Assets/Scripts/FireBullet.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class FireBullet : MonoBehaviour
 {
    [SerializeField] private GameObject bullet;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField]  private float _canFire=-1f;
+   [SerializeField]  private float _fireRate =0.15f;
+      [SerializeField]  private float lives=3;
+    
 void FireBullets(){
+      
         Instantiate(bullet,transform.position,Quaternion.identity);
+        _canFire = Time.time+_fireRate;
 }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time >_canFire){
             FireBullets();
         }
     }
